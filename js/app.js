602130c6e6cc0591cd73db200c777cfdb5289a16
AndresSanchez2021 <anfsanchezcu@unal.edu.co>
2022-06-17 01:40:28 -0500

    se uso fileReader para cargar las imagnes, fix lista contactos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-let persona = function(n, t, g, f, d){
+let contactos = [];
+let picture="";
+const lector = new FileReader;
+
+lector.addEventListener('load', (event) => {
+    document.getElementById("imgPreview").src = event.target.result;
+})
+
+const persona = function(n, t, g, f, d){
     this.nombre = n;
     this.tel =t;
     this.grupo = g;
@@ -6,12 +14,12 @@ let persona = function(n, t, g, f, d){
     this.descripcion = d;
 
 }
-
 function agregarPersona(){
     let forma = document.forms["forma"];
     if(forma["nombre"].value !="" && forma["tel"].value !=""){
-        let aux = new persona(forma["nombre"].value,forma["tel"].value,forma["grupo"].value,forma["agregar-foto"].value,forma["descripcion"].value)
-        mostrar();
+        
+        let aux = new persona(forma["nombre"].value,forma["tel"].value,forma["grupo"].value,picture,forma["descripcion"].value)
+        
         return aux;
     }
     return false;
@@ -43,24 +51,36 @@ function mostrarContacto(id){
         let foto = p.foto;
         if(foto=="")
             foto="statics/user.png"
+        else{
+            lector.readAsDataURL(foto);
+        }
 
         console.log(p);
-        document.getElementById("card-user").innerHTML=`<img src="${foto}" alt="" class="d-block img-fluid img-user">
+        document.getElementById("card-user").innerHTML=`<img src="${foto}" alt="" class="d-block img-fluid img-user" id="imgPreview">
         <h5 class="card-title ">${p.nombre}</h5>
         <h6 class="card-subtitle text-muted ">${p.grupo}</h6>
         <p class="card-text">${p.descripcion}</p>
         <a href="" class="btn btn-primary btn-lg"><i class="fa fa-phone"></i><strong>${p.tel}</strong></a>`
     }
-    
-
-    
 }
 
-let contactos = [];
+
+document.getElementById("agregar-foto").addEventListener("change",(event)=>{
+    
+    const file = event.target.files[0];
+    picture=file;
+    console.log("file: ", file);
+
+})
+
+
+
+
 document.getElementById("crearContacto").addEventListener("click",()=>{
     let a =agregarPersona();
     if(a!=false){
         contactos.push(a);
+        mostrar();
     }
 })
 document.getElementById("eliminar").addEventListener("click",()=>{console.log(contactos)})
